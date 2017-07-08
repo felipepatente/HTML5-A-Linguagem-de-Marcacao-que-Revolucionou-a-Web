fc19be2d4cf644ca7d9179014246f64867c1668d
felipepatente <felipepatente@yahoo.com.br>
2017-07-08 12:14:50 -0300

    Comentario do atributo strokeRect

--- HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capitulo 4 - Canvas/aula4.js
+++ HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capitulo 4 - Canvas/aula4.js
@@ -8,16 +8,26 @@ function chamarFuncao(){
 	//Esse atributo destina-se a definir a espessura das linhas, em pixel, do elemento gráfico criado	
 	ctx.lineWidth = 2;
 
-
+	/*
+	  strokeRect(x,y,l,h);	
+	  Esse método destina-se a desenhar o contorno de um retângulo. Os parâmetros x e y definem as coordenadas 
+	  do canto superior esquerdo do retângulo e os parâmetros l e h definem, respectivamente, a largura e a altura,
+	  em pixel, do retângulo
+	*/
 	ctx.strokeRect(30,30,80,40);
 	
+
 	ctx.strokeStyle = '#00F';
 	ctx.lineWidth = 4;
-	ctx.strokeRect(130,100,140,90);
+	ctx.strokeRect(10,10,120,90);
 
 	ctx.strokeStyle = '#000';
 	ctx.lineWidth = 2;
+
+	
 	ctx.fillStyle = 'rgba(0,255,0,0.4)';
-	ctx.fillRect(150,60,200,70)
-	ctx.strokeRect(148,58,204,74);
+	
+	
+	ctx.fillRect(148,20,200,70)
+	ctx.strokeRect(148,20,150,70);
 }
