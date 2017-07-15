893852006286c8a3b8862bb6fea8cf5fe2b688c8
felipepatente <felipepatente@yahoo.com.br>
2017-07-15 16:43:45 -0300

    Trabalhando com mapas

--- HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capítulo 7 - Geolocalização/aula7.js
+++ HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capítulo 7 - Geolocalização/aula7.js
@@ -66,6 +66,7 @@ function sucesso(position){
 // ----------------------------------------------------------------------
 
 
+// ----------------------------------------------------------------------
 function inspecionarErro(){
 
 	if(navigator.geolocation){
@@ -106,6 +107,69 @@ function erro(err){
 function sucess(position){
 	document.getElementById('mssg').innerHTML = "Provoque um erro, negando...";
 }
+
+// ----------------------------------------------------------------------
+
+
+function mostrarMapa(){
+
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(suces, error);
+	}else{
+		alert("Este navegador não suporta a funcionalidade Geolocation");
+	}
+}
+
+function error(err){
+
+	switch(err.code){
+		case 1:
+			var mensagemErro = "A permissão para obter a sua posição foi negada.";
+			break;
+
+		case 2:
+			var mensagemErro = "Não foi possível estabeler uma conexão para obter á sua posição.";
+			break;
+
+		case 3:
+			var mensagemErro = "Tempo esgotado."
+			break;
+
+		default:
+			var mensagemErro = "Não foi possível obter sua posição";
+			break;
+	}
+
+	var codigoErro = err.code;
+	var mensagem = "Ocorreu um erro na determinação da posição: <br>";
+	mensagem += "Código do erro: " + codigoErro + "<br>";
+	mensagem += "Mensagem: " + mensagemErro;
+	document.getElementById('mssgg').innerHTML = mensagem;
+}
+
+function suces(position){
+
+	var mapa = document.getElementById("mapa");
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+	var url = "http://maps.google.com/maps/api/staticmap?center";
+	url += latitude + "," + longitude;
+	url += "&zoom=15&size=400x400";
+	url += "&markers=color:red|"+ latitude + "," + longitude;;
+	url += "&sensor=true";
+	mapa.src = url;
+	mapa.style.display = "block";
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
