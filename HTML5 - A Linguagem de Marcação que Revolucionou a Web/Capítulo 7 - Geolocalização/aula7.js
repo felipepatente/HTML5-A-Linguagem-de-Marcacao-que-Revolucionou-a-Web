409fa9195cef487b83efbbf049f37d8778f1f15e
felipepatente <felipepatente@yahoo.com.br>
2017-07-15 15:18:59 -0300

    Obtendo latitude, longitude, altitude, precisão, pricisãoAltitude, direção, velocidade e tempo

--- HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capítulo 7 - Geolocalização/aula7.js
+++ HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capítulo 7 - Geolocalização/aula7.js
@@ -10,6 +10,27 @@ function chamarLocalizacao(){
 function obterPosicao(){
 	
 	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(sucessos);
+	}else{
+		alert("Este navegador não suporta a funcionalidade Geolocation");
+	}
+}
+
+function sucessos(position){
+
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+
+	var mensagem = "As suas coordenadas são: <br>";
+	mensagem += "Latitude: " + latitude + "<br>";
+	mensagem += "Longitude: " + longitude;
+	document.getElementById('msg').innerHTML = mensagem;
+}
+
+
+function propriedadesPosition(){
+
+	if(navigator.geolocation){
 		navigator.geolocation.getCurrentPosition(sucesso);
 	}else{
 		alert("Este navegador não suporta a funcionalidade Geolocation");
@@ -20,9 +41,47 @@ function sucesso(position){
 
 	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
+	var altitude = position.coords.altitude;
+	var precisao = position.coords.accuracy;
+	var precisaoAltitude = position.coords.altitudeAccuracy;
+	var direcao = position.coords.heading;
+	var velocidade = position.coords.speed;
+	var tempo = position.timestamp;
 
-	var mensagem = "As suas coordenadas são: <br>";
-	mensagem += "Latitude: " + latitude + "<br>";
-	mensagem += "Longitude: " + longitude;
-	document.getElementById('msg').innerHTML = mensagem;
+	var mensagem = "As propriedades de Position são: <br>";
+	mensagem += "Latidute" + latitude + "<br>";
+	mensagem += "Longitude" + longitude + "<br>";
+	mensagem += "Presição das coordenadas: " + precisao + " metros<br>";
+	mensagem += "<br>";
+	mensagem += "Altitude: " + altitude + "<br>";
+	mensagem += "Presição da altitude: " + precisaoAltitude + " metros<br>";
+	mensagem += "<br>";
+	mensagem += "Direção: " + direcao + " graus<br>";
+	mensagem += "velocidade: " + velocidade + "<br>";
+	mensagem += "Tempo: " + tempo + " milissegundos";
+	document.getElementById('msgg').innerHTML = mensagem;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
