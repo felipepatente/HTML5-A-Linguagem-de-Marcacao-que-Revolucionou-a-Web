2839364c6b3e2df20d0115876ad397a1b9243bd9
felipepatente <felipepatente@yahoo.com.br>
2017-07-15 14:40:11 -0300

    Obtendo a latidute e a longitude com auxilio do método getCurrentPosition() do objeto geolocation

--- HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capítulo 7 - Geolocalização/aula7.js
+++ HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capítulo 7 - Geolocalização/aula7.js
@@ -5,3 +5,24 @@ function chamarLocalizacao(){
 		alert("Este navegador não suporta a funcionalidade Geolacation");
 	}
 }
+
+
+function obterPosicao(){
+	
+	if(navigator.geolocation){
+		navigator.geolocation.getCurrentPosition(sucesso);
+	}else{
+		alert("Este navegador não suporta a funcionalidade Geolocation");
+	}
+}
+
+function sucesso(position){
+
+	var latitude = position.coords.latitude;
+	var longitude = position.coords.longitude;
+
+	var mensagem = "As suas coordenadas são: <br>";
+	mensagem += "Latitude: " + latitude + "<br>";
+	mensagem += "Longitude: " + longitude;
+	document.getElementById('msg').innerHTML = mensagem;
+}
