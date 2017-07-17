739b054e97a55a14e47144e6afb8695b51ae9745
felipepatente <felipepatente@yahoo.com.br>
2017-07-17 15:57:22 -0300

    Exemplo de armazenamento utilizando o localStorage
    É um mecaniso que foi projetado com a finalidade de armazenar dados que
    persistem em diferentes janelas ou abas do navegador

---
+++ HTML5 - A Linguagem de Marcação que Revolucionou a Web/Capítulo 8 - Armazenamento de Dados/aula8.js
@@ -0,0 +1,47 @@
+function salvarDado(name) {
+	
+	var valor = document.forms['dadoform'].dadovalue.value;
+	var itemStorage = localStorage.setItem('dataStorage',valor);
+	var valorItemStorage = localStorage.getItem('dataStorage');
+
+	if(!valor){
+		alert("Entre um valor para o dado a armazenar.");
+	}else{
+		mensagem = "Um dado com o nome dataStorage com o valor " + valorItemStorage + "\n";
+		mensagem += "foi armazenado";
+		alert(mensagem);
+	}
+}
+
+function lerDado(name){
+	var valorItemStorage = localStorage.getItem('dataStorage');
+	if(!valorItemStorage){
+		alert("Não existe valor para ser lido");
+	}else{
+		mensagem = "Um dado com o nome dataStorage com o valor " + valorItemStorage + "\n";
+		mensagem += "foi lido";
+		alert(mensagem);
+	}
+}
+
+function apagarDado(name){
+
+	localStorage.removeItem('dataStorage');
+	alert("Dado com o nome dataStorage foi apagado");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
